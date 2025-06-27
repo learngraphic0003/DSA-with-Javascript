cb944776d8dceee508f9a0e7a523ac2533bf42e4
shubham <learngraphic0003@gmail.com>
2025-06-27 17:35:55 +0530

    update

--- 4patternprogramming.js
+++ 4patternprogramming.js
@@ -1,4 +1,15 @@
 
+let prompt = require('prompt-sync')()
+
+let N = Number(prompt("enter the number"))
+
+
+for( i = 1 ; N >= i;i++){
+    process.stdout.write("*")
+}
+
+
+
 
 
 
@@ -11,3 +22,12 @@
     process.stdout.write("*")
     process.stdout.write("*")
     process.stdout.write("*")
+
+
+    for( i = 1 ; i <= N ;i++){
+     for(j = 1; j <= N ;J++ )
+     {
+        process.stdout.write("*")
+     }
+     console.log()
+}
