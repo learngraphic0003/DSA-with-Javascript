5ecb3c58c26324bcb6f74b5158130daae2d0ccc3
shubham <learngraphic0003@gmail.com>
2025-06-29 16:43:25 +0530

    update

--- 4patternprogramming.js
+++ 4patternprogramming.js
@@ -4,9 +4,9 @@ let prompt = require('prompt-sync')()
 let N = Number(prompt("enter the number"))
 
 
-for( i = 1 ; N >= i;i++){
-    process.stdout.write("*")
-}
+// for( i = 1 ; N >= i;i++){
+//     process.stdout.write("*")
+// }
 
 
 
@@ -14,20 +14,52 @@ for( i = 1 ; N >= i;i++){
 
 
 
-    console.log("*")
-    console.log("*")
-    console.log("*")
+//     console.log("*")
+//     console.log("*")
+//     console.log("*")
 
-    process.stdout.write("*")
-    process.stdout.write("*")
-    process.stdout.write("*")
-    process.stdout.write("*")
+//     process.stdout.write("*")
+//     process.stdout.write("*")
+//     process.stdout.write("*")
+//     process.stdout.write("*")
+
+
+//     for( i = 1 ; i <= N ;i++){
+//      for(j = 1; j <= N ;j++ )
+//      {
+//         process.stdout.write("*")
+//      }
+//      console.log()
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     for( i = 1 ; i <= N ;i++){
+//      for(j = 1; j <= i ;j++ )
+//      {
+//         process.stdout.write("*")
+//      }
+//      console.log()
+// }
 
 
     for( i = 1 ; i <= N ;i++){
-     for(j = 1; j <= N ;J++ )
+        let ascii = 65
+     for(j = 1; j <= i ;j++ )
+
      {
-        process.stdout.write("*")
+        process.stdout.write (String.fromCharCode(ascii)+" " )  
+        ascii++ 
      }
      console.log()
 }
